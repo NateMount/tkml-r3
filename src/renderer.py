36f8906214ab7f1dde07575a62582efbe8ebaef6
Nate Mount <ngmount2021@gmail.com>
2022-03-27 03:00:12 -0400

    Redefined functions in runnder and renderer

--- src/renderer.py
+++ src/renderer.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3.10
 
-def _render(path:str) -> None:
+def render(path:str) -> None:
 	"""Generates an executable file from tkml source file"""
 	raise NotImplementedError
--- src/runner.py
+++ src/runner.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3.10
 
-def _run(path:str) -> None:
+def run(path:str) -> None:
 	"""Used to live run a tkml file"""
 	raise NotImplementedError
