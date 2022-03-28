047f48e870d441d2b27f727f3b094ca69b57210b
Nate Mount <ngmount2021@gmail.com>
2022-03-28 16:06:51 -0400

    Added skeleton xrender support framework

--- src/__init__.py
+++ src/__init__.py
@@ -3,4 +3,5 @@
 from .runner import run as _run
 from .util import _warn, _help
 from .renderer import render as _render
+from .renderer import xrender as _xrender
 from .debug import debug as _debug
--- src/renderer.py
+++ src/renderer.py
@@ -3,3 +3,7 @@
 def render(path:str) -> None:
 	"""Generates an executable file from tkml source file"""
 	raise NotImplementedError
+
+def xrender(path:str, form:str) -> None:
+	"""Generates an executable file from tkml source file"""
+	raise NotImplementedError
