3eede988bca4ef606657d5005a775ebb1a10f1f5
Nate Mount <ngmount2021@gmail.com>
2022-04-11 02:16:31 -0400

    Removed xRender

--- src/renderer.py
+++ src/renderer.py
@@ -3,7 +3,3 @@
 def render(path:str) -> None:
 	"""Generates an executable file from tkml source file"""
 	raise NotImplementedError
-
-def xrender(path:str, form:str) -> None:
-	"""Generates an executable file from tkml source file"""
-	raise NotImplementedError
