7294970219243d6a6ec12eae3b7897afe58c9db7
Nate Mount <ngmount2021@gmail.com>
2022-04-11 02:14:22 -0400

    Updated __init__ to emulate new file structure

--- src/__init__.py
+++ src/__init__.py
@@ -3,5 +3,5 @@
 from .runner import run as _run
 from .util import _warn, _help
 from .renderer import render as _render
-from .renderer import xrender as _xrender
+from .builder import build as _build
 from .debug import debug as _debug
