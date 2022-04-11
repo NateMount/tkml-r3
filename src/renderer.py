74e5e086f9f23d896f4f44f6f6428e0254231541
Nate Mount <ngmount2021@gmail.com>
2022-04-11 16:09:08 -0400

    Added initial read in framework to renderer

--- src/renderer.py
+++ src/renderer.py
@@ -1,5 +1,20 @@
 #! /usr/bin/env python3.10
 
+import os, sys
+from .util import _read, _warn, _debug
+
 def render(path:str) -> None:
 	"""Generates an executable file from tkml source file"""
-	raise NotImplementedError
+	
+	_debug("FUNCTION [_render] CALLED")
+
+	if not (_data := _read(path)):
+		_warn("No data recovered from file")
+		sys.exit()
+
+	_debug(f"DATA : {_data}")
+
+	if 'init' not in _data:
+		_warn("Init frame not found")
+		sys.exit()
+
