c3a91501e4619f84ec072068177c8d85e0a4fbd9
Nate Mount <ngmount2021@gmail.com>
2022-08-13 23:24:26 -0400

    Added skeleton framework to render function

--- src/renderer.py
+++ src/renderer.py
@@ -3,7 +3,7 @@
 import os, sys
 from .util import _read, _warn, _debug
 
-def render(path:str) -> None:
+def render(path:str, form:str) -> None:
 	"""Generates an executable file from tkml source file"""
 	
 	_debug("FUNCTION [_render] CALLED")
@@ -17,4 +17,14 @@ def render(path:str) -> None:
 	if 'init' not in _data:
 		_warn("Init frame not found")
 		sys.exit()
+	
 
+	match form:
+		case 'html':
+			_rend_html(_data)
+		case 'cli':
+			_rend_cli(_data)
+		case 'rui':
+			_rend_rui(_data)
+		case _:
+			_warn("Unrecognized rendering format")
